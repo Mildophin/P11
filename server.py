6582af95a882ddbb7f162cf7390170e72d100f07
Milan Viricel <viricel.milan@gmail.com>
2022-03-06 13:41:30 +0100

    Correction de l'utilisation de points illimitée des clubs

--- server.py
+++ server.py
@@ -46,9 +46,15 @@ def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
-    competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
+    availablePoints = int(club.get('points', None))
+    # Debug bug/Les clubs ne peuvent pas utiliser plus de points que ce qu'ils ont
+    if placesRequired > availablePoints:
+        message = f"You don't have enough points to book {placesRequired} places."
+        flash(message, 'error')
+        return render_template('welcome.html', club=club, competitions=competitions)
+    competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - placesRequired
     flash('Great-booking complete!')
-    return render_template('welcome.html', club=club, competitions=competitions)
+    return render_template('welcome.html', club=club, competitions=competitions, availablePoints=availablePoints)
 
 
 # TODO: Add route for points display
